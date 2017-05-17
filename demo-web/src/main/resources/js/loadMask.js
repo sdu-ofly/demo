86692f405f9d92f83e931f0f782eb13d929fd015
Logan_715 <77381218@qq.com>
2017-05-17 08:45:02 +0800

    loadMask 去除css依赖

--- demo-web/src/main/resources/js/loadMask.js
+++ demo-web/src/main/resources/js/loadMask.js
@@ -3,27 +3,51 @@ var loadMask = (function(){
 	var _imgWidth = 28;
 	var _imgHeight = 28;
 	var loading = function(id) {
-		var loading = $('<span class="maskImg"><span>');
-		var html = $('<div id="maskDiv" class="mask"></div>');
+		var loading = $('<span ><span>');//class="maskImg"
+		var html = $('<div id="maskDiv" ></div>');//class="mask"
 		html.append(loading);
 		$('#'+id).append(html);
-		
+		// 初始遮罩层样式
+		for(var name in _cssOptions.mask) {
+			html.css(name,_cssOptions.mask[name]);
+		}
 		// 遮罩层位置
 		html.css("height",$('#'+id).height());
 		html.css("width",$('#'+id).width());
 		html.css("top",$('#'+id).offset().top);
 		html.css("left",$('#'+id).offset().left);
 		var z_index = $('#'+id).css('z-index');
-		if(isNaN(z_index)) {//自动auto
+		if(isNaN(z_index)) {// 自动auto
 			z_index = 0;
 		}
 		html.css('z-index', z_index+1);
+		//初始遮罩层图片样式
+		for(var name in _cssOptions.maskImg) {
+			loading.css(name,_cssOptions.maskImg[name]);
+		}
 		// 遮罩层图表位置
 		loading.css("margin-top",($('#'+id).height()-_imgHeight)/2);
 		loading.css("margin-left",($('#'+id).width()-_imgWidth)/2);
 	}
 	var remove = function(id) {
 		$('#'+id).find('#maskDiv').remove();
+	}
+	
+	var _cssOptions = {
+			mask:{
+				position: 'absolute',
+				top: '0',
+				left:'0',
+				opacity	: '0.5',
+				background: 'gray'
+			},
+			maskImg: {
+			    background: 'url(../../img/demo/loading.gif)',
+			    height: '28px',
+			    width: '28px',
+			    display: 'block',
+			    position: 'fixed'
+			}
 	}
 	
 	_this.loading = loading;
